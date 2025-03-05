09ead42c4ae4bb6400ea6a8d75e7fdfd1ee8b452
Kreverson <kreversonjulho@gmail.com>
2025-03-05 20:34:34 -0300

    Adicionada solução vazia spring boot

---
+++ src/test/java/artapp/points_api/PointsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package artapp.points_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
