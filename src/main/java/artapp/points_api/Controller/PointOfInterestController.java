fd6b4a4333f8fb3fbd3ba4bb320ed74540e9e24c
Kreverson <kreversonjulho@gmail.com>
2025-03-05 21:01:35 -0300

    Criado controller e DTO para os pontos de interesse

--- src/main/java/artapp/points_api/Controller/PointOfInterestController.java
+++ src/main/java/artapp/points_api/Controller/PointOfInterestController.java
@@ -1,6 +1,8 @@
 package artapp.points_api.Controller;
 
 import artapp.points_api.Controller.dto.CreatePointOfInterest;
+import artapp.points_api.entity.PointOfInterest;
+import artapp.points_api.repository.PointOfInterestRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PointOfInterestController {
 
+    private final PointOfInterestRepository repository;
+
+    public PointOfInterestController(PointOfInterestRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping("/points-of-interests")
     public ResponseEntity<Void> createPOI(@RequestBody CreatePointOfInterest dto) {
+        repository.save(new PointOfInterest(dto.name(), dto.x(), dto.y()));
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/artapp/points_api/entity/PointOfInterest.java
+++ src/main/java/artapp/points_api/entity/PointOfInterest.java
@@ -19,8 +19,7 @@ public class PointOfInterest {
     public PointOfInterest() {
     }
 
-    public PointOfInterest(Long id, String name, Long x, Long y) {
-        this.id = id;
+    public PointOfInterest(String name, Long x, Long y) {
         this.name = name;
         this.x = x;
         this.y = y;
