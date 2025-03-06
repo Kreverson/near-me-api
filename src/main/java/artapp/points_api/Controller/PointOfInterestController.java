0d5a040728f8656d08fd009ef16e83bba249c39e
Kreverson <kreversonjulho@gmail.com>
2025-03-05 21:09:29 -0300

    Criado listagem de pontos de interesse

--- src/main/java/artapp/points_api/Controller/PointOfInterestController.java
+++ src/main/java/artapp/points_api/Controller/PointOfInterestController.java
@@ -3,10 +3,10 @@ package artapp.points_api.Controller;
 import artapp.points_api.Controller.dto.CreatePointOfInterest;
 import artapp.points_api.entity.PointOfInterest;
 import artapp.points_api.repository.PointOfInterestRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PointOfInterestController {
@@ -21,6 +21,15 @@ public class PointOfInterestController {
     public ResponseEntity<Void> createPOI(@RequestBody CreatePointOfInterest dto) {
         repository.save(new PointOfInterest(dto.name(), dto.x(), dto.y()));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/points-of-interests")
+    public ResponseEntity<Page<PointOfInterest>> ListPOI(@RequestParam (name = "page", defaultValue = "0") Integer page,
+                                                         @RequestParam (name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+        var body = repository.findAll(PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(body);
     }
 
 }
