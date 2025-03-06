70741cff2d1e31d33ccaecacc1b51f0f40219559
Kreverson <kreversonjulho@gmail.com>
2025-03-05 23:24:50 -0300

    Adicionado funcionalidade de busca pontos de interesse no plano cartesiano

--- src/main/java/artapp/points_api/Controller/PointOfInterestController.java
+++ src/main/java/artapp/points_api/Controller/PointOfInterestController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class PointOfInterestController {
 
@@ -29,6 +31,21 @@ public class PointOfInterestController {
 
         var body = repository.findAll(PageRequest.of(page, pageSize));
 
+        return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/near-me")
+    public ResponseEntity<List<PointOfInterest>> ListPOI(@RequestParam ("x") Long x,
+                                                         @RequestParam ("y") Long y,
+                                                         @RequestParam("dmax") Long dmax) {
+
+        var xMin = x - dmax;
+        var xMax = x + dmax;
+        var yMin = y - dmax;
+        var yMax = y + dmax;
+
+        var body = repository.findNearMe(xMin, xMax, yMin, yMax);
+        
         return ResponseEntity.ok(body);
     }
 
